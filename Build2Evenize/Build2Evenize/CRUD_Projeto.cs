8d805fde3504cc40c028e1dcdfa0ebc57014bd4d
Paulo Andrade <ispg2018102350@ispgaya.pt>
2021-12-04 16:50:35 +0000

    Project CRUD

---
+++ Build2Evenize/Build2Evenize/CRUD_Projeto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Build2Evenize
+{
+    public partial class CRUD_Projeto : Form
+    {
+        public CRUD_Projeto()
+        {
+            InitializeComponent();
+        }
+    }
+}
